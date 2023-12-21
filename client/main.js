ff1701d569a1cf41e1f343b9a6957a2bf16892b8
dldnlee <sungeun8877@gmail.com>
2023-12-21 21:42:47 +0900

    useless functions removed and working pokemon card slider

--- client/main.js
+++ client/main.js
@@ -1,25 +1,7 @@
 import { getNode as $, insertLast, insertFirst } from "./modules/index.js";
 
 
-const image = $('.image-card');
-
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  autoplay:false,
-  parallax: true,
-  speed: 1000,
-  effect: 'cards',
-  createElements: true,
-  navigation:{
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  }
-  // delay: 1000,
-}) 
-
-
-
-
+// Extracting Pokemon Data
 async function getPokemonData(card_number) {
   const stringCardNumber = card_number.toString()
   const response = await fetch(`https://api.pokemontcg.io/v2/cards/swsh4-${stringCardNumber}`);
@@ -27,13 +9,13 @@ async function getPokemonData(card_number) {
   return response.json();
 }
 
+// Extracting Image Source from Pokemon Data
 async function getCardImageSource(card_number) {
   const data = await getPokemonData(card_number);
   return data.data.images.small;
 }
 
-
-
+// Swiper Slide Template Creator
 function createSwiperSlide(imageSource, alt) {
   return( /* html */ `
   <div class="swiper-slide">
@@ -43,23 +25,41 @@ function createSwiperSlide(imageSource, alt) {
   )
 }
 
-
-const imageSrc1 = await getCardImageSource(130);
-const imageSrc2 = await getCardImageSource(132);
-const imageSrc3 = await getCardImageSource(133);
-const imageSrc4 = await getCardImageSource(134);
-const swiperWrapper = $('.swiper-wrapper');
-
-// console.log(typeof await getCardImageSource(130)); // result string 
-
-function addNewPokemonCard(imageSource, alt) {
-
-  const slide = createSwiperSlide(imageSource, alt)
-  insertLast(swiperWrapper, slide);
+// Swiper Initialization
+function initializeSwiper() {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: false,  
+    parallax: true,
+    speed: 1000,
+    effect: 'cards',
+    createElements: true,
+    navigation:{
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    // delay: 1000,
+  }) 
 }
 
-addNewPokemonCard(imageSrc1, 'hello');
-addNewPokemonCard(imageSrc2, 'hello');
-addNewPokemonCard(imageSrc3, 'hello');
-addNewPokemonCard(imageSrc4, 'hello');
+
+
+// Running the Whole Process
+async function addAll() {
+  const swiperWrapper = $('.swiper-wrapper');
+  let imageSrc;
+  let slide;
+  for(let i=50;i<100; i++){
+    imageSrc = await getCardImageSource(i);
+    slide = createSwiperSlide(imageSrc, 'hello')
+    insertLast(swiperWrapper, slide);
+    
+  }
+  initializeSwiper();
+}
+
+
+
+addAll();
+
 
