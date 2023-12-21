2ca2f2e78bf5a8845dd2c62d35b5449659fca8b5
dldnlee <sungeun8877@gmail.com>
2023-12-21 21:05:07 +0900

    working card animation and bring source

--- client/main.js
+++ client/main.js
@@ -8,7 +8,13 @@ const swiper = new Swiper('.swiper', {
   autoplay:false,
   parallax: true,
   speed: 1000,
-  delay: 1000,
+  effect: 'cards',
+  createElements: true,
+  navigation:{
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+  // delay: 1000,
 }) 
 
 
@@ -21,37 +27,30 @@ async function getPokemonData(card_number) {
   return response.json();
 }
 
-async function setCardImage(imageDom, card_number) {
-  const data = await getPokemonData(card_number);
-  const dataImageSource = data.data.images.small;
-  imageDom.src = dataImageSource;
-
-  createSwiperSlide(dataImageSource, 'hello');
-}
-
 async function getCardImageSource(card_number) {
   const data = await getPokemonData(card_number);
-  return await data.data.images.small;
+  return data.data.images.small;
 }
+
+
 
 function createSwiperSlide(imageSource, alt) {
   return( /* html */ `
   <div class="swiper-slide">
-    newone
     <img src=${imageSource} alt=${alt}>
   </div>
   `
   )
 }
 
+
 const imageSrc1 = await getCardImageSource(130);
 const imageSrc2 = await getCardImageSource(132);
 const imageSrc3 = await getCardImageSource(133);
 const imageSrc4 = await getCardImageSource(134);
 const swiperWrapper = $('.swiper-wrapper');
-// let imageSource = await getCardImageSource(140);
 
-console.log(typeof await getCardImageSource(130));
+// console.log(typeof await getCardImageSource(130)); // result string 
 
 function addNewPokemonCard(imageSource, alt) {
 
@@ -63,25 +62,4 @@ addNewPokemonCard(imageSrc1, 'hello');
 addNewPokemonCard(imageSrc2, 'hello');
 addNewPokemonCard(imageSrc3, 'hello');
 addNewPokemonCard(imageSrc4, 'hello');
-
-
-async function text() {
-  addNewPokemonCard(await getCardImageSource(141), 'hello');
-  addNewPokemonCard(await getCardImageSource(145), 'hello');
-  addNewPokemonCard(await getCardImageSource(147), 'hello');
-}
-
-
-
-// setTimeout(text, 4000);
-
-// createSwiperSlide()
-// setCardImage();
-
-// async function getImageSource(){
-//   return new Promise((resolve, reject) => {
-
-//   })
-// }
-// getPokemonData('150');
 
